1bfde4f9156628068e7e41c1debc5132bb755218
sejdm <shane.dmell@gmail.com>
2018-05-25 19:50:57 +0530

    Cleaned

--- src/Shak.hs
+++ src/Shak.hs
@@ -45,10 +45,9 @@ printChunk :: (Int, [Line]) -> IO ()
 printChunk (_, []) = return ()
 printChunk (n, l:ls) = do
   B.putStrLn $ "Screen: " `B.append` B.pack (show  n)
-  --colorStrLn Vivid Cyan Dull Black (view lastCharacter l)
   B.putStrLn ""
   colorStrLn Vivid Cyan Dull Black (view act l)
-  colorStrLn Vivid Blue Dull Black ("    " `B.append` view scene l)
+  colorStrLn Vivid Blue Dull Black (pad $ view scene l)
   B.putStrLn ""
   colorStrLn Vivid Yellow Dull Black (("Characters present: " `B.append`) $ showStringList $ view charactersPresent l)
   B.putStrLn ""
@@ -56,22 +55,30 @@ printChunk (n, l:ls) = do
     Stage -> B.putStrLn "" >> colorStrLn Vivid Green Dull Black (view line l) >> B.putStrLn ""
     _ -> colorStrLn Vivid Red Dull Black (view lastCharacter l `B.append` ":") >> mapM_ (printLine) (if view line l /= "."  && view line l /= ". " then l : ls else ls)
 
+
 printLine l = case view lineType l of
   Stage -> B.putStrLn "" >> colorStrLn Vivid Green Dull Black (pad $ view line l) >> B.putStrLn ""
-  --Stage -> colorStrLn Vivid Green Dull Black (view line l)
   _ -> B.putStrLn $ pad $ view line l
+
+
 
 showStringList [] = ""
 showStringList [x] = x
 showStringList [x,y] = x `B.append` ", " `B.append` y
 showStringList (x:xs) = x `B.append` ", " `B.append` showStringList xs
 
+
+
 showLine l = case l ^. lineType of
   Stage -> colorStrLn Vivid Green Dull Black (B.pack $ show $ view line l)
   _ -> B.putStrLn (view line l)
 
+
+
 showChunk [] = ""
 showChunk (l:ls) = view act l `B.append` "\n" `B.append` view scene l `B.append` "\n" `B.append` (B.pack . show) (view charactersPresent l) `B.append` "\n" `B.append` view lastCharacter l `B.append` "\n\n" `B.append` B.unlines (map (view line) ls)
+
+
 
 
 colorStrLn fgi fg bgi bg str = do
@@ -82,13 +89,6 @@ colorStrLn fgi fg bgi bg str = do
 
 pad = B.append "    "
 
---searchLine s (xs, ys) = (xs, filter (\(_, (_,ss')) -> any (\s' -> s `B.isInfixOf` view line s') ss') ys)
-searchLine' s ys = span (\(_, (_,ss')) -> not (any (\s' -> s `B.isInfixOf` view line s') ss')) ys
 searchLine s ss' = any (\s' -> s `B.isInfixOf` view line s') ss'
 
-{-
-searchLine s a@(xs, ys) = case search s ys of
-  (_, []) -> a
-  (ds, rs) -> (reverse ds ++ xs, rs)
--}
 
